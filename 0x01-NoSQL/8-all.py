9c6dbacf69ab73cc53f7ad3f6b8645e9b837e099
fikayosalu <salufikayo@gmail.com>
2025-05-06 17:34:40 +0100

    Task 7 error

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -2,15 +2,14 @@
 """ This script uses pymongo to query a database """
 
 
-if __name__ == '__main__':
-    def list_all(mongo_collection):
-        """
-        Lists all documents in a MongoDB collection.
+def list_all(mongo_collection):
+    """
+    Lists all documents in a MongoDB collection.
 
-        Args:
-        mongo_collection: pymongo collection object
+    Args:
+    mongo_collection: pymongo collection object
 
-        Returns: A list of documents,
-        or an empty list if no documents exist.
-        """
-        return list(mongo_collection.find())
+    Returns: A list of documents,
+    or an empty list if no documents exist.
+    """
+    return list(mongo_collection.find())
